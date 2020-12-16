cee0205dd526c2a5de1066f07bcf35107558d23e
Antonio Ochoa <antonhub00@gmail.com>
2020-12-16 00:46:56 -0600

    Creates and updates Firebase collection/document/field

--- TestFirebasePreview/ViewController.swift
+++ TestFirebasePreview/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let db = Firestore.firestore()
+        
+        // Creates if not exists or overwrite if does exist
+        // db.collection("users").document("user1").setData(["name": "John", "lastname": "Thompson"])
+        
+        // Updates a field or set of fields
+        db.collection("users").document("user1").setData(["lastname": "Wick"], merge: true)
     }
 
 
